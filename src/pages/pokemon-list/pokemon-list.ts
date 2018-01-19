e42d18f604a7d8797d10ddf977770156a74019dc
Thibaut DUFOUR <thibaut.dufour@outlook.com>
2018-01-19 14:44:07 +0100

    searchbar added

--- src/pages/pokemon-list/pokemon-list.ts
+++ src/pages/pokemon-list/pokemon-list.ts
@@ -4,8 +4,13 @@ import { Observable } from "rxjs/Observable";
 import { map } from "rxjs/operators";
 
 import { PokemonService } from "../../app/core/pokemon.service";
+import { LanguageService } from "../../app/core/language.service";
 
 import { PokemonDetailPage } from "../pokemon-detail/pokemon-detail";
+
+import { translations_de } from "../../app/shared/translations.de";
+import { translations_en } from "../../app/shared/translations.en";
+import { translations_fr } from "../../app/shared/translations.fr";
 
 
 @Component({
@@ -13,10 +18,22 @@ import { PokemonDetailPage } from "../pokemon-detail/pokemon-detail";
   templateUrl: 'pokemon-list.html',
 })
 export class PokemonListPage {
+  
   pokemons: any[];
   searchValue: string;
+  toggled: boolean;
+  searchTerm: string = '';
+  item: string[];
+  language: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private pokemonService: PokemonService) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private pokemonService: PokemonService, languageService: LanguageService) {
+    // Hide search and show title
+    this.toggled = false;
+
+    // Get language for searchbar
+    this.language = languageService.getLanguage();
+
   }
 
   ionViewDidLoad() {
@@ -24,8 +41,21 @@ export class PokemonListPage {
     this.getAllPokemons().subscribe();
   }
   
-  filterPokemon(pokemon: any): boolean {
-    return pokemon.id.toString().includes(this.searchValue) || pokemon.name.includes(this.searchValue.toLowerCase());
+  toggleSearch() {
+    
+    this.toggled = this.toggled ? false : true;
+  }
+
+  filterPokemon(pokemon: any) {
+    // Search result according to the app language 
+    switch (this.language) {
+      case 'en':
+        return pokemon.id.toString().includes(this.searchValue) || translations_en.get(pokemon.name).includes(this.searchValue.toLowerCase());
+      case 'fr':
+        return pokemon.id.toString().includes(this.searchValue) || translations_fr.get(pokemon.name).includes(this.searchValue.toLowerCase());
+      case 'de':
+        return pokemon.id.toString().includes(this.searchValue) || translations_de.get(pokemon.name).includes(this.searchValue.toLowerCase());
+    }
   }
   
   private getAllPokemons(): Observable<any> {
